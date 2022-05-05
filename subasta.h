0dcc88252f425318c0bbba10e23d6ac5c17e19e9
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-05-05 11:14:47 -0300

    Update header

--- subasta.h
+++ subasta.h
@@ -3,13 +3,11 @@
 
 #include <iostream>
 #include <vector>
-#include <string>
 
 using namespace std;
 
 class Persona {
 private:
-    vector <string> nombre;
     string nombrePersona;
 
 public:
@@ -20,29 +18,60 @@ public:
         nombrePersona = nombre;
     }
 
-    void print();
-    void setPersona(string nombre);
+    string getPersona();
+    void setPersona(string);
+    void printPersona();
 };
 
 
 class Oferta {
 private:
     Persona ofertante;
-    float monto;
+    float montoOfertado;
 
 public:
     Oferta(){
         Persona p("Agustin");
         ofertante = p;
-        monto = 0;
+        montoOfertado = 0.0;
     }
 
     Oferta(Persona newOfrt, float mto) {
         ofertante = newOfrt;
-        monto = mto;
+        montoOfertado = mto;
     }
 
+    float getMontoOfertado();
     void printOferta();
+};
+
+
+class Lote {
+private:
+    Oferta mayorOferta;
+    int numeroLote;
+    string nombreLote;
+
+public:
+    Lote() {
+        Persona per("no-name");
+        Oferta ofertaVacia(per, 0.0);
+
+        mayorOferta = ofertaVacia;
+        numeroLote = 0;
+        nombreLote = "Lote without name";
+    }
+    Lote(Oferta oferta, int nro, string n) {
+         mayorOferta = oferta;
+         numeroLote = nro;
+         nombreLote = n;
+    }
+
+    Oferta getMayorOferta();
+    int getNumeroLote();
+    string getNombreLote();
+    void printLote();
+    bool validarMayor(Oferta);
 };
 
 #endif
