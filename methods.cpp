722cbdca3f21b257a664785c59c87051e14e4a37
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-05-05 11:15:05 -0300

    Update methods file

--- methods.cpp
+++ methods.cpp
@@ -1,17 +1,50 @@
 #include "subasta.h"
 
-// Metodos para la clase persona
-void Persona :: print() {
+// Metodos para la clase Persona
+string Persona :: getPersona() {
+    return nombrePersona;
+}
+
+void Persona :: printPersona() {
     cout << '\t' << "Nombre: " << nombrePersona << endl;
 }
 
-void Persona :: setPersona(string nombre) {
-    nombrePersona = nombre;
+void Persona :: setPersona(string newName) {
+    nombrePersona = newName;
+    //cout << '\t' << "Nueva persona seteada: " << nombrePersona << endl;
 }
 
-// Metodos para la clase oferta
+// Metodos para la clase Oferta
+float Oferta :: getMontoOfertado() {
+  return montoOfertado;
+}
+
 void Oferta :: printOferta() {
-    cout << '\t' << "Ofertante" << endl;
-    ofertante.print();
-    cout << '\t' << "monto: " << monto << endl;
+    cout << '\t' << "- Ofertante" << endl;
+    ofertante.printPersona();
+    cout << '\t' << "Monto: " << montoOfertado << endl;
+}
+
+// Metodos para la clase Lote
+bool Lote :: validarMayor(Oferta newOferta) {
+    return newOferta.getMontoOfertado() > mayorOferta.getMontoOfertado();
+}
+
+Oferta Lote :: getMayorOferta() {
+    return mayorOferta;
+}
+
+int Lote :: getNumeroLote() {
+    return numeroLote;
+}
+
+string Lote :: getNombreLote() {
+    return nombreLote;
+}
+
+void Lote :: printLote() {
+    cout << '\t' << "- Lote" << '\n' << endl;
+    cout << '\t' << "Nombre de lote: " << nombreLote << endl;
+    cout << '\t' << "Numero de lote: " << numeroLote << '\n' << endl;
+    mayorOferta.printOferta();
 }
