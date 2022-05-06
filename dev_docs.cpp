4a466d16fddd0af92cdc645381a4074f0a06575f
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-05-06 13:40:03 -0300

    Add a new file with useful annotations

---
+++ dev_docs.cpp
@@ -0,0 +1,12 @@
+    /**
+     * Hasta el momento mi programa cuenta con todas las clases
+     * y metodos enunciadas en la descripcion del programa.
+     *
+     * Ademas ya hemos probado que funcionan correctamente tanto
+     * en el archivo testing_classes como en el main.
+     *
+     * Cosas por solucionar:
+     * - Pensar como se lleva a cabo el inicio de la subasta, en
+     * el metodo subastar.
+     * - Terminar de reflexionar sobre el ciclo de vida del programa.
+     */
