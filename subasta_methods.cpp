4bfa68531433e8d9208c1a05bd7120369dc7e36c
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-05-06 13:37:11 -0300

    Add a new file with subasta class methods

---
+++ subasta_methods.cpp
@@ -0,0 +1,39 @@
+#include "subasta.h"
+
+// Metodos para la clase Subasta
+
+void Subasta :: setLotes(vector<Lote> nuevosLotes) {
+    lotes = nuevosLotes;
+}
+
+void Subasta :: setLote(Lote l, int nroLote) {
+    lotes[nroLote] = l;
+}
+
+void Subasta :: ingresarOferta(Persona p, float monto, Lote l, int nroLote) {
+    Oferta nuevaOferta(p, monto);
+
+    if (lotes[nroLote].validarMayor(nuevaOferta) == 1) {
+        cout << "La oferta hecha por $" << monto << " fue aceptada" << endl;
+        lotes[nroLote].setMayorOferta(nuevaOferta);
+    } else {
+        cout << "La oferta hecha por $" << monto << " no fue aceptada" << endl;
+    }
+}
+
+vector<Lote> Subasta :: getLotes() {
+    return lotes;
+}
+
+int Subasta :: getCantidadLotes() {
+    return cantidad;
+}
+
+void Subasta :: mostrarLotes() {
+  cout << "Se imprimen los lotes en la subasta: " << endl;
+
+  for (int i = 0; i < cantidad; i++) {
+      cout << '\n' << '\n' << i+1 << ") -------------" << endl;
+      lotes[i].printLote();
+  }
+}
