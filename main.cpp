d2d06c636517d0b8f88911efe932024dbe50fe0f
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-05-06 13:38:31 -0300

    Update the main file with some of the definitive code

--- main.cpp
+++ main.cpp
@@ -1,53 +1,59 @@
 #include "subasta.h"
 
-int main()
-{
-        // Probando la clase Persona y sus metodos
-    cout << "1) Vamos a inicializar una persona..." << endl;
-    Persona p1("Agustin");
-    cout << '\t' << "Persona inicializada!: " << p1.getPersona() << endl;
+void siguientePaso() {
+  string respuesta = "";
 
-    cout << endl << "Ahora vamos a mostrar quien es la persona: " << endl;
-    p1.printPersona();
+  cout << '\n' << "Para continar ingrese cualquier valor" << endl;
+  cin >> respuesta;
+}
 
-    cout << endl << "Vamos a setear un nuevo valor a la clase persona: " << endl;
-    p1.setPersona("Celeste");
-    cout << '\t' << "Persona inicializada!: " << p1.getPersona() << endl;
+int main() {
+    /* Creamos algunas personas las cuales
+        participaran en la subasta */
+    Persona p1("Agustin"), p2("Celeste"), p3("Vicente"), p4("Ninito");
 
-    cout << endl << "Veamos que nombre tiene ahora la clase: " << endl;
-    p1.printPersona();
+    /* Creamos algunos lotes y luego
+        los ultilizamos en nuestra clase Subasta */
+    Lote lote1(0, "Laptop Dell Latitude i7"),
+         lote2(1, "Monitor Samsung Odyssey G3"),
+         lote3(2, "Microprocesador Ryzen 5"),
+         lote4(3, "Teclado Apple Magic"),
+         lote5(4, "Smartphone Iphone 13 mini"),
+         lote6(5, "Cafetetera De'Longhi Dedica"),
+         lote7(6, "Laptop MacBook pro 13\""),
+         lote8(7, "Micro ordenador Raspberry Pi 4 Model B");
 
-        // Probando la clase Oferta
-    cout << endl << "2) Vamos a inicializar una oferta..." << endl;
-    Oferta oft(p1, 1200);
-    cout << '\t' << "Oferta inicializada!" << endl;
+    vector<Lote> lotes(8);
 
-    cout << endl << "Ahora vamos a mostrar la oferta: " << endl;
-    oft.printOferta();
+    lotes[0] = lote1;
+    lotes[1] = lote2;
+    lotes[2] = lote3;
+    lotes[3] = lote4;
+    lotes[4] = lote5;
+    lotes[5] = lote6;
+    lotes[6] = lote7;
+    lotes[7] = lote8;
 
-        // Probando la clase Lote
-    cout << endl << "3) Ahora vamos a inicializar la clase Lote con el constructor sin parametros: " << endl;
-    Lote lote1;
-    cout << '\t' << "Lote inicializado!" << endl;
+    Subasta techItems(8);
 
-    cout << endl << "Veamos el contenido de nuestra clase lote: " << endl;
-    lote1.printLote();
+    cout << " - SUBASTA DE ARTICULOS TECNOLOGICOS - " << endl;
 
-    cout << endl << "Ahora vamos a inicializar la clase Lote con el constructor parametrizado: " << endl;
-    Lote lote2(oft, 1, "Remera");
-    cout << '\t' << "Lote inicializado!" << endl;
+    techItems.setLotes(lotes);
 
-    cout << endl << "Veamos el contenido de nuestra nueva clase lote: " << endl;
-    lote2.printLote();
+    siguientePaso();
 
-    cout << endl << "Accedemos a los campos del lote mediante los metodos get: "
-    << lote2.getNombreLote() << ", " << lote2.getNumeroLote() << endl;
+    cout << '\n' << "- Probamos metodo mostrarLotes" << endl;
+    techItems.mostrarLotes();
 
-    cout << endl << "Ahora vamos a inicializar una ofeta nueva para poder utilizar el metodo de validacion.";
-    cout << endl << "Esta oferta sera mayor, con lo cual el metodo de validacion deberia retornar true (1)" << endl;
-    Oferta oft2(p1, 1500);
+    siguientePaso();
 
-    cout << endl << "resultado: " << lote2.validarMayor(oft2) << endl;
+    cout << '\n' << "- Probamos metodo ingresarOferta" << endl;
+    techItems.ingresarOferta(p2, 205000.0, lote5, 4);
+
+    siguientePaso();
+
+    cout << '\n' << "- Probamos metodo mostrarLotes para checkear la nueva oferta" << endl;
+    techItems.mostrarLotes();
 
     return 0;
 }
